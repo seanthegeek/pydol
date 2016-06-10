268beada3f9d0293bfe11482b30fe8654839ab92
Sean Whalen <whalenster@gmail.com>
2016-06-10 10:37:55 -0400

    Modernize packaging and submit to PyPI
    https://pypi.python.org/pypi/pydol

--- setup.py
+++ setup.py
@@ -1,57 +1,99 @@
 #!/usr/bn/env python
 # -*- coding: utf-8 -*-
 
-"""Setup script for pydol"""
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
 
-# pylint: disable=F0401
-# pylint: disable=E0611
+# Always prefer setuptools over distutils
+from setuptools import setup
+# To use a consistent encoding
+from codecs import open
+from os import path
 
-from distutils.core import setup
+here = path.abspath(path.dirname(__file__))
 
-__author__ = "Sean Whalen"
-__copyright__ = "Copyright 2016 %s" % __author__
-__license__ = "Apache 2.0"
-license_text = """Copyright 2016 Sean Whalen
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+setup(
+    name='pydol',
 
-   http://www.apache.org/licenses/LICENSE-2.0
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='1.0.3',
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License."""
+    description='A pythonic interface to the U.S. Department of Labor API',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/seanthegeek/pydol',
+
+    # Author details
+    author='Sean Whalen',
+    author_email='whalenster@gmail.com',
+
+    # Choose your license
+    license='Apache 2.0',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Legal Industry',
+        'Operating System :: OS Independent',
 
 
-def get_requirements():
-    with open("requirements.txt") as requirements_file:
-        return map(lambda line: line.trim(), requirements_file.read().split('\n'))
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: Apache Software License',
 
-def get_readme():
-    with open("README.rst") as readme_file:
-        return readme_file.read()
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
 
-setup(name="pydol",
-      version="1.0.3",
-      description="A pythonic interface to the U.S. Department of Labor API",
-      license=license_text,
-      author="Sean Whalen",
-      url='https://github.com/seanthegeek/pydol',
-      author_email="whalenster@gmail.com",
-      packages=['pydol', 'pydol.test'],
-      long_description=get_readme(),
-      install_requires=get_requirements(),
-      classifiers=[
-          'Intended Audience :: Developers',
-          'Intended Audience :: Education',
-          'Intended Audience :: Science/Research',
-          'Intended Audience :: Legal Industry',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 3',
-          'Topic :: Education',
-          'Topic :: Scientific/Engineering :: Information Analysis',
-          'Topic :: Utilities'])
+    # What does your project relate to?
+    keywords='DoL USDoL BLS statistics API',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    packages=['pydol'],
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['requests', 'xmltodict'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'test': ['nose']
+    }
+)
